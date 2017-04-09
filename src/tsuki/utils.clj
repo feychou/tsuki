d796721e3a288cebd6bc43295eb173920f3e4b02
feychou <federica.recanatini@gmail.com>
2017-04-10 00:08:38 +0200

    Adjust indentation in utils

--- src/tsuki/utils.clj
+++ src/tsuki/utils.clj
@@ -1,23 +1,23 @@
 (ns tsuki.utils
   (:gen-class)
   (:require [java-time :as t]
-  					[clojure.string :as s]))
+            [clojure.string :as s]))
 
 (defn get-day [x]
-	(t/format "yyyy-MM-dd" (t/minus (t/local-date) (t/days x))))
+  (t/format "yyyy-MM-dd" (t/minus (t/local-date) (t/days x))))
 
 (defn last-year []
-	(- (read-string (first (s/split (t/format "yyyy-MM-dd" (t/local-date)) #"-"))) 1))
+  (- (read-string (first (s/split (t/format "yyyy-MM-dd" (t/local-date)) #"-"))) 1))
 
 (def yesterday (get-day 1))
 
 (def day-before-yesterday (get-day 2))
 
 (defn add-nulls [num-vector]
-	(map #(str 0 %) num-vector))
+  (map #(str 0 %) num-vector))
 
 (defn random-date []
-	(str 
-		(rand-nth (range 1997 (last-year))) "-"
-		(rand-nth (concat (add-nulls (range 1 9)) (range 10 12))) "-"
-		(rand-nth (concat (add-nulls (range 1 9)) (range 10 28)))))
+  (str 
+    (rand-nth (range 1997 (last-year))) "-"
+    (rand-nth (concat (add-nulls (range 1 9)) (range 10 12))) "-"
+    (rand-nth (concat (add-nulls (range 1 9)) (range 10 28)))))
