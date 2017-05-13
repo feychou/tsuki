e77b36ff81eb5c5dbc8fd947611ba2e7c9106fa2
feychou <federica.recanatini@gmail.com>
2017-05-13 11:09:09 +0200

    Add timeout to make cron job more reliable

--- src/tsuki/jobs.clj
+++ src/tsuki/jobs.clj
@@ -5,7 +5,11 @@
             [chime :refer [chime-at]]
             [clj-time.core :as t]
             [clj-time.periodic :refer [periodic-seq]]
-            [taoensso.faraday :as far])
+            [taoensso.faraday :as far]
+            [clojure.core.async
+             :as a
+             :refer [<! go timeout]]
+            [environ.core :refer [env]])
   (:import [org.joda.time DateTimeZone]))
 
 (def interval 
@@ -20,4 +24,6 @@
               (println "Chiming at " time)
               (println (str "All ids " (far/scan utils/dynamo-creds :tsuki-users)))
               (doseq [subscriber (far/scan utils/dynamo-creds :tsuki-users)]
-                     (actions/send-astropic-template (:fb-id subscriber) (actions/get-today-astro-pic) :toast true :menu false)))))
+                      (go
+                        (<! (timeout 2000))
+                        (actions/send-astropic-template (:fb-id subscriber) (actions/get-today-astro-pic) :toast true :menu false))))))
